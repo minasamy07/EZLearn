56a201b7955dd773a5eba68c9367115c5c4e73a4
minasamy07 <gohncina60@gmail.com>
2024-04-03 17:39:47 +0200

    PP

--- src/router/admin.js
+++ src/router/admin.js
@@ -2,9 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const auth = require("../middleware/admin-auth");
 const Admin = require("../models/admin");
+const multer = require("multer");
 const router = new express.Router();
 
-//create Admin
+//create admin
 router.post("/admins", cors(), async (req, res) => {
   const admin = new Admin(req.body);
   try {
@@ -19,7 +20,7 @@ router.post("/admins", cors(), async (req, res) => {
   }
 });
 
-// //login Admin
+// //login admin
 
 router.post("/admins/login", async (req, res) => {
   try {
@@ -53,19 +54,20 @@ router.post("/admins/logout", auth, async (req, res) => {
   }
 });
 
-//get loged user
+//get loged admin
 
 router.get("/admins/me", auth, async (req, res) => {
   res.send(req.admin);
 });
 
-//get all users
+//get all admins
 router.get("/admins/all", async (req, res) => {
   const admin = await Admin.find();
 
   res.send(admin);
 });
 
+//update personal data
 router.patch("/admins/update", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdate = ["name", "email", "password"];
@@ -86,5 +88,65 @@ router.patch("/admins/update", auth, async (req, res) => {
     res.status(400).send(e);
   }
 });
+
+// add profile picture
+
+const upload = multer({
+  limits: { fileSize: 1000000 },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.endsWith(".jpg" || ".jpeg" || ".png")) {
+      return cb(new Error("upload a photo"));
+    }
+    cb(undefined, true);
+  },
+});
+
+router.post(
+  "/admins/profilePicture",
+  auth,
+  upload.single("avatar"),
+  async (req, res) => {
+    req.admin.avatar = req.file.buffer;
+    await req.admin.save();
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
+
+// get profile picture
+
+router.get("/admins/getPP", auth, async (req, res) => {
+  try {
+    const admin = req.admin;
+    if (!admin || !admin.avatar) {
+      throw new error();
+    }
+
+    res.set("Content-type", "image/png" || "image/jpeg" || "image/jpg");
+    res.send(admin.avatar);
+  } catch (e) {
+    res.status(404).send();
+  }
+});
+
+//delete Profile Picture
+
+router.delete("/admins/deletePP", auth, async (req, res) => {
+  req.admin.avatar = undefined;
+  await req.admin.save();
+  res.send();
+});
+
+// //delete admin
+// router.delete("/admins/delete", auth, async (req, res) => {
+//   try {
+//     await req.admin.remove();
+//     res.send(req.admin);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
 
 module.exports = router;
